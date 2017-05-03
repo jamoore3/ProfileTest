97667f64de52f18c35cdf5dcee144995893031f2
John Moore <Admin@johnmoosmacbook>
2017-05-03 16:26:34 -0400

    First commit

--- ProfileTest/ViewController.swift
+++ ProfileTest/ViewController.swift
@@ -13,6 +13,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        print("Passport Profile Test")
     }
 
     override func didReceiveMemoryWarning() {
